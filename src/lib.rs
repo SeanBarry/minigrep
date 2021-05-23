a7cfb9b0c88e41e28fcff9727d97e9f800ea044a
Sean Barry <se@nbarry.co.uk>
2021-05-23 22:02:53 +0100

    fix: implementing iterators in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub struct Config {
     pub query: String,
@@ -8,22 +8,32 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 
-
 impl Config {
-    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        // first arg is the program name, so skip it
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a filename"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-    
-        Ok(Config { query, filename, case_sensitive })
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
     let results = if config.case_sensitive {
@@ -71,16 +81,11 @@ Trust me.";
     }
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) ->Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -91,7 +96,6 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
         if line.to_lowercase().contains(&query) {
             results.push(line);
         }
-        
     }
 
     results
